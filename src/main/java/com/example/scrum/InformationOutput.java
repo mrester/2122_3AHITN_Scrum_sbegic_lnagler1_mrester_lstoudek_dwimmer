027c73c0c2742d564a4d8a8b7aa2571333dc9f99
Daniel <dwimmer1@htl-steyr.ac.at>
2022-05-06 09:06:17 +0200

    InformationOutput added

---
+++ src/main/java/com/example/scrum/InformationOutput.java
@@ -0,0 +1,36 @@
+package com.example.scrum;
+
+public class InformationOutput {
+
+
+    /**
+     * Funktionsbeschreibung
+     *	whichPlayer dient zur ausgabe welcher Spieler drann ist
+     */
+    public void whichPlayer(){ // Welcher Spieler drann ist
+
+    }
+    /**
+     * Funktionsbeschreibung
+     * isHit dient dazu auszugeben ob etwas getroffen wurde oder nicht
+     */
+    public void isHit(){
+
+    }
+
+    /**
+     * Funktionsbeschreibung
+     * whichFieldHit dient dazu wenn ein Feld getroffen wurde auszugeben welches getroffen wurde
+     */
+    public void whichFieldHit(){
+
+    }
+    /**
+     * Funktionsbeschreibung
+     * whichAbility dient dazu auszugeben welches Fähigkeit ausgewählt wurde
+     */
+    public void whichAbility(){
+
+    }
+
+}
