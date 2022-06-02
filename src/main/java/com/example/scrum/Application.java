df11075dbf88c99064a918a9b2896c2d3537dd61
Daniel <dwimmer1@htl-steyr.ac.at>
2022-06-02 14:22:23 +0200

    Application FXML edit

--- src/main/java/com/example/scrum/Application.java
+++ src/main/java/com/example/scrum/Application.java
@@ -1,22 +1,34 @@
 package com.example.scrum;
 
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class Application extends javafx.application.Application {
+    private static Scene scene;
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("Menue.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello!");
+        scene = new Scene(loadFXML("Menue"), 640, 650);
         stage.setScene(scene);
         stage.show();
+
+    }
+
+    static void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml));
+    }
+
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource(fxml + ".fxml"));
+        return fxmlLoader.load();
     }
 
     public static void main(String[] args) {
         launch();
+
     }
 }
