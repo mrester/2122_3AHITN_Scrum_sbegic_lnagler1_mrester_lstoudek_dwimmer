311afc3fe83aa9ea17bc6dd31c177a8a7f862106
sbegic <sbegic2003@gmail.com>
2022-06-03 08:56:24 +0200

    Controller und Menue changed

--- src/main/java/com/example/scrum/Controller_Menue.java
+++ src/main/java/com/example/scrum/Controller_Menue.java
@@ -47,7 +47,7 @@ public class Controller_Menue {
         logs.ProssesPlayerInfo(s2.name, land2);
 
         if (name1.equals("") || name2.equals("") || name1.equals(name2) || land1.equals("null") || land2.equals("null") || land1.equals(land2)) {
-            logs.inputError();
+            logs.InputError();
         }else {
             switchToPlayfield();
         }
